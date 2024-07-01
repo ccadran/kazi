61aac0a0ff778695382ce755366a1e7de93b1125
ccadran <clariocadran33@gmail.com>
2024-07-01 21:45:45 +0200

    feat(app) : fetch supabase data

--- app.vue
+++ app.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+const supabase = useSupabaseClient();
+
+const fecthData = async () => {
+  const data = await supabase.from("categories").select("*");
+  console.log(data.data);
+};
+onMounted(() => {
+  fecthData();
+});
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
