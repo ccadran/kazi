f4e43c52d6523f659e7e3815d573f579dbf4848c
ccadran <clariocadran33@gmail.com>
2024-08-13 09:58:05 +0200

    feat(layout) : start nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,18 @@
 <script lang="ts" setup></script>
 
 <template>
-  <nav class="nav">
-    <h3>Navbar</h3>
+  <nav class="navbar">
+    <div class="navbarContent">
+      <a href="/">
+        <img src="/icons/kazi-logo.svg" alt="" height="50px" />
+      </a>
+      <div class="navbarLinks">
+        <a href="/categories">CATEGORIES</a>
+        <a href="/quiz">QUIZ</a>
+        <a href="/community">community</a>
+        <a href="/profile">my Account</a>
+      </div>
+    </div>
   </nav>
   <slot />
   <footer class="footer">
@@ -10,4 +20,17 @@
   </footer>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.navbar {
+  > .navbarContent {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    > .navbarLinks {
+      display: flex;
+      gap: 20px;
+      text-transform: uppercase;
+    }
+  }
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/supabase", "@pinia/nuxt"],
+  css: ["~/assets/styles/index.scss"],
   supabase: {
     redirect: false,
   },
