b0f99bceecd58ea9b02256408023bebd8a47df7c
ccadran <clariocadran33@gmail.com>
2024-08-14 12:50:56 +0200

    style(home): testQuiz

---
+++ components/ui/TestQuizDifficulty.vue
@@ -0,0 +1,29 @@
+import { type } from '../../.nuxt/types/imports';
+<script lang="ts" setup>
+const props = defineProps({
+  difficulty: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="testQuizDifficulty">
+    <p class="_3-xl">{{ `${difficulty} difficulty` }}</p>
+    <img src="/icons/arrow.svg" alt="" width="60px" />
+  </div>
+</template>
+
+<style lang="scss">
+.testQuizDifficulty {
+  display: flex;
+  width: 40%;
+  justify-content: space-between;
+  padding: 22px 0;
+  border-top: 1px solid var(--c-black);
+  border-bottom: 1px solid var(--c-black);
+  p {
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -8,14 +8,58 @@ categories.value = data.value;
 </script>
 
 <template>
-  <HeroHome />
-  <Test />
-  <div>Ma page d'accueil</div>
-  <div v-for="category in categories">
-    <h1>{{ category.title }}</h1>
-    <NuxtLink :to="`/categories/${category.id}`">Voir les sous-thèmes</NuxtLink>
-  </div>
-  <NuxtLink :to="'/categories'">Voir l'ensemble des catégories</NuxtLink>
+  <main>
+    <HeroHome />
+
+    <div class="testQuiz">
+      <div class="inner1">
+        <p>Not so sure of the utility?</p>
+      </div>
+      <div class="innner2">
+        <p>START A TEST QUIZ RIGHT NOW</p>
+      </div>
+      <div class="testQuizChoice">
+        <UiTestQuizDifficulty difficulty="easy" />
+        <UiTestQuizDifficulty difficulty="medium" />
+        <UiTestQuizDifficulty difficulty="hard" />
+      </div>
+    </div>
+    <div>Ma page d'accueil</div>
+    <div v-for="category in categories">
+      <h1>{{ category.title }}</h1>
+      <NuxtLink :to="`/categories/${category.id}`"
+        >Voir les sous-thèmes</NuxtLink
+      >
+    </div>
+    <NuxtLink :to="'/categories'">Voir l'ensemble des catégories</NuxtLink>
+  </main>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+main {
+  margin: 0 60px;
+}
+.testQuiz {
+  display: flex;
+  flex-direction: column;
+  font-size: 48px;
+  text-transform: uppercase;
+  > .inner1 {
+    margin-top: 92px;
+    width: 40%;
+    margin-right: auto;
+  }
+  > .innner2 {
+    margin-top: 132px;
+    width: 100%;
+    p {
+      width: 40%;
+      margin-left: auto;
+    }
+  }
+
+  > .testQuizChoice {
+    margin-top: 180px;
+  }
+}
+</style>
